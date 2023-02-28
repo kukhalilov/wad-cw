804f1f13e6fd24f9e92f9373ec5e800a4fec55e8
kukhalilov <kukhalilov@gmail.com>
2023-02-28 17:13:58 +0500

    feat: add API project with Book and Author models

---
+++ WebAPI/WebAPI/Models/Author.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Surname is required")]
+        [MaxLength(50, ErrorMessage = "Surname cannot exceed 100 characters")]
+        public string Surname { get; set; }
+
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ WebAPI/WebAPI/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+
+        [Required(ErrorMessage = "Author is required")]
+        public Author Author { get; set; }
+
+
+        [Required(ErrorMessage = "Publish date is required")]
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; }
+
+
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+
+
+        [Range(1, int.MaxValue, ErrorMessage = "Page count must be a positive integer")]
+        public int PageCount { get; set; }
+
+
+        public bool IsAvailable { get; set; }
+    }
+}
